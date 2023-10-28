afa934a9c2e2b955eec451d44b27b955dfe4986f
Vivek Murugesan <40669174+vivekmu@users.noreply.github.com>
2023-10-28 17:08:20 +0530

    Update crontest.py

--- crontest.py
+++ crontest.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 with open('/root/crontest.txt','w') as file2:
     file2.write('Cront test')
+
+# crontab -e
+#*/1 * * * * /usr/bin/python3 /root/crontest.py >> out.txt  2>&1
